81ab07b666879c46fb81237b69af82fc84929005
localTOAST <localtoast@eggandjam.com>
2013-11-14 22:42:41 -0800

    Add a ping endpoint

--- image.js
+++ image.js
@@ -17,6 +17,11 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 
+server.get('/ping', function (req, res, next) {
+  res.send(200, {still: 'alive'})
+  return next();
+}
+
 server.get('/tp/:tracking', function (req, res, next) {
   var current = 0;
   var total = img.length;
