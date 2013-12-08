5129a86223ff56ca7814349da33a404425355a1a
localTOAST <localtoast@eggandjam.com>
2013-12-07 17:55:00 -0800

    remove from heroku, add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+set :application, 'tracker'
+set :repo_url, 'git@github.com:juliangiuca/tracker.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+ set :deploy_to, '/data/tracker'
+# set :scm, :git
+
+# set :format, :pretty
+# set :log_level, :debug
+# set :pty, true
+
+ set :linked_files, %w{config/settings.json}
+ #set :linked_dirs, %w{bin logs tmp/pids tmp/cache tmp/sockets}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 3
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+      #if test("[ -f #{shared_path}/tmp/pids/unicorn.pid ]")
+        #execute "cat #{shared_path}/tmp/pids/unicorn.pid"
+        #execute "kill -USR2 `cat #{shared_path}/tmp/pids/unicorn.pid`"
+      #else
+        #info "No unicorn process found"
+      #end
+    end
+  end
+
+  task :pull_down_secret_files do
+    on roles(:all) do
+      execute "wget --user=#{ENV['BITBUCKET_USER']} --password='#{ENV['BITBUCKET_PASSWORD']}' -q -N https://bitbucket.org/localtoast/secret-files/raw/master/emailer/settings.production.json -O /data/tracker/shared/config/settings.json"
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+  after :started, 'deploy:pull_down_secret_files'
+
+end
